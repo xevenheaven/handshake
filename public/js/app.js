b90089859b0bb5f8e70beba289a71a6ba81b486c
Elysia Ong <elysia.owx@gmail.com>
2016-11-28 20:08:10 +0800

    Implement sign in screen

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,12 @@
 var App = angular.module('App', ['ui.router', 'ngMaterial']);
 
 App.config(function($stateProvider) {
+	var splash = {
+		name: 'splash',
+		url: '/',
+		templateUrl: './index.html'
+	};
+
 	var signin = {
 		name: 'signin',
 		url: '/signin',
@@ -13,8 +19,16 @@ App.config(function($stateProvider) {
 		templateUrl: './templates/signup.html'
 	};
 
+	var welcome = {
+		name: 'welcome',
+		url: '/welcome',
+		templateUrl: './templates/welcome.html'
+	};
+
+	$stateProvider.state(splash);
 	$stateProvider.state(signin);
 	$stateProvider.state(signup);
+	$stateProvider.state(welcome);
 });
 
 App.controller('SplashScreenController', function SplashScreenController($scope, $state) {
@@ -24,5 +38,27 @@ App.controller('SplashScreenController', function SplashScreenController($scope,
 
 	$scope.signup = function () {
 		$state.go('signup');
+	};
+});
+
+App.controller('SignInController', function SignInController($scope, $state) {
+	$scope.email;
+	$scope.password;
+
+	$scope.back = function () {
+		$state.go('splash');
+	};
+
+	$scope.submit = function () {
+		dpd.users.login({
+			username: $scope.email,
+			password: $scope.password
+		}, function(session, error) {
+			if (error) {
+				alert('Login failed!');
+			} else {
+				$state.go('welcome');
+			}
+		});
 	};
 });
